56b24df14b27219118442ccef4cadfde7a24f4fa
Vlad <vladshoshin19@gmail.com>
2026-05-12 20:19:15 +0700

    first commit dropdown

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,20 @@
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+    const valueElement = dropdown.querySelector('.dropdown__value');
+    const listElement = dropdown.querySelector('.dropdown__list');
+    const items = dropdown.querySelectorAll('.dropdown__item');
+
+    valueElement.addEventListener('click', () => {
+        listElement.classList.toggle('dropdown__list_active');
+    });
+
+    items.forEach(item => {
+        item.addEventListener('click', (event) => {
+            event.preventDefault();
+            listElement.classList.remove('dropdown__list_active');
+            const link = item.querySelector('.dropdown__link');
+            valueElement.textContent = link.textContent.trim();
+        });
+    });
+});
